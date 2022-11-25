e7ba4f984a9c2a3c361cbeb27181f6e40543130c
Yuru Zhou <yuruzhou@Yurus-MBP.attlocal.net>
2022-11-24 22:09:48 -0800

    project setup

---
+++ Instagram/Controllers/Main Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Yuru Zhou on 11/24/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
